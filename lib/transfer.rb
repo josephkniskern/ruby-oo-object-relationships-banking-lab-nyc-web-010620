1080941d3b10fda7df7a595bbb4fffbdf4c65064
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-01-09 15:10:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transfer
 
   attr_accessor :sender, :reciever, :amount, :status
 
-  def initialize(sender, reciever, amount)
+  def initialize(sender, receiver, amount)
     @receiver = receiver
     @sender = sender
     @amount = amount
